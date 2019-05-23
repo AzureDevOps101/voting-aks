2d705555b8234e414fd501097f73f146d6023250
jackyzhou <jackyzhou@leansoftx.com>
2019-05-23 12:31:59 +0800

    clearning

--- calculator/test/helpers.js
+++
@@ -1,7 +0,0 @@
-var supertest = require('supertest');
-var chai = require('chai');
-var app = require('../server');
-
-global.app = app;
-global.expect = chai.expect;
-global.request = supertest(app);
